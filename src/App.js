bccf01dd51d26d81a0a96e4c0cfea3dde38d42dd
REMASOFT2468 <Linusgodremark@gmail.com>
2024-04-22 12:25:22 +0100

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/Meanbody.css 	new file:   src/Meanbody.js 	new file:   src/image/Linkedin.png 	new file:   src/image/coffee2.jpg 	new file:   src/image/fb.png 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Meanbody from "./Meanbody";
+
+function App() {
+  return (
+    <div className="App">
+      <Meanbody />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Meanbody.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Meanbody.css";
+import img from "./image/fb.png";
+import img1 from "./image/Linkedin.png";
+import img3 from "./image/coffee2.jpg";
+
+function Meanbody() {
+  return (
+    <div className="man">
+      <div className="man1">
+        <h1>coffee</h1>
+        <ul className="man2">
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>contactUs</li>
+        </ul>
+        <div>
+          <button className="man3">SIGN IN</button>
+        </div>
+      </div>
+
+      <div className="man6">
+        <div className="man4">
+          <div>
+            <img src={img} />
+          </div>
+          <div>
+            <img src={img1} />
+          </div>
+          <div>
+            <img src={img} />
+          </div>
+        </div>
+        <div>
+          <div className="man5">
+            <div className="man10">
+              <h1>A HOT CUP </h1>
+              <h1 className="man11">OF HAPPINESS</h1>
+            </div>
+            <h5 className="man12">
+              Boost your productivity and build your mood with glasss of coffee
+              in the morning coffee has a stimulating effect
+            </h5>
+            <div className="man13">
+              <button className="man14">Order Now</button>
+              <div className="man7">
+                <h3>$15.00</h3>
+                <div className="man9"></div>
+                <h5 className="man8">regular price</h5>
+                <div className="man15">
+                  <img src={img3} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Meanbody;
